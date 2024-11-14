7163a51425e64b2087cce8a036104d4bf8465b87
aluda <aluda.galla@ucalgary.ca>
2024-11-14 14:18:43 -0700

    changed some things in login, signup gui layouts

--- src/main/java/com/example/LoginController.java
+++ src/main/java/com/example/LoginController.java
@@ -1,26 +1,57 @@
 package com.example;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.ToggleButton;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+
 
 import java.io.IOException;
 
 public class LoginController {
+
     @FXML
-    private TextField usernameField; //linked to corresponding components
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField passwordTextField;
+    @FXML
+    private ToggleButton toggleButton;
 
-    public void userSignIn() { // called when user clicks "sign in"
-        //stuff will happen
-
+    @FXML
+    void toggleButton(ActionEvent event) { //visibility of password
+        // Check the state of the toggle button
     }
-    public void userSignUp(MouseEvent event) throws IOException { // when the user clicks "signup"
-        //loading signup fxml file
+
+
+    @FXML
+    void userSignIn(ActionEvent event) { // called when user clicks "sign in"
+        // Stuff will happen
+    }
+
+    @FXML
+    void forgotPassword(MouseEvent event) {
+        // Handle forgot password action here
+    }
+
+    @FXML
+    public void toSignUpPage(MouseEvent event) throws IOException { // when the user clicks "signup"
+        // Create a new FXMLLoader instance and load the signup FXML
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("signup.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400); // loading the scene
-
-
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400); // Loading the scene
+        // Get the stage from the event
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Signup");
+        stage.setScene(scene);
+        stage.show();
     }
+
 }
+
--- src/main/java/com/example/SignUpController.java
+++ src/main/java/com/example/SignUpController.java
@@ -1,6 +1,49 @@
 package com.example;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 public class SignUpController {
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private PasswordField confirmPasswordField; // maybe
+
+    @FXML
+    private Button signUpButton; // Linked to the sign-up Button in FXML
 
 
+    @FXML
+    void toLoginScreen(MouseEvent event) {
+        try {
+            // Load the FXML for the login screen
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400); // Create a new scene for the login page
+
+            // Get the current stage from the event
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            // Set the new scene to the current stage (navigate to the login screen)
+            currentStage.setTitle("Login");
+            currentStage.setScene(scene);
+            currentStage.show();
+        } catch (IOException e) {
+            // Handle the IOException
+        }
+    }
 }
