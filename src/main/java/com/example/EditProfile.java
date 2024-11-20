078a4df7a4f465fcb5079f709017ba6219ee1887
aluda <aluda.galla@ucalgary.ca>
2024-11-20 02:41:11 -0700

    potential layout for edit profile screen

--- src/main/java/com/example/EditProfile.java
+++ src/main/java/com/example/EditProfile.java
@@ -1,19 +1,37 @@
 package com.example;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class EditProfile {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EditProfile implements Initializable {
 
     @FXML
-    private Button backButton;
+    private Button viewProfileButton;
+
+    @FXML
+    private Label logoutLabel;
+
+    @FXML
+    private ComboBox<String> displayStatusComboBox;
 
 
     @FXML
     private void toProfilePage() {
         Utility.loadPage("profilepage.fxml",
-                (Stage) backButton.getScene().getWindow());
+                (Stage) viewProfileButton.getScene().getWindow());
     }
 
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        displayStatusComboBox.setItems(FXCollections.observableArrayList("Public", "Private"));
+    }
 }
--- src/main/java/com/example/ProfileController.java
+++ src/main/java/com/example/ProfileController.java
@@ -1,13 +1,21 @@
 package com.example;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-public class ProfileController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProfileController implements Initializable {
     @FXML
     private Button backButton;
     @FXML
@@ -20,8 +28,13 @@ public class ProfileController {
 
     @FXML
     private void toEditProfilePage() {
-        Utility.loadPage("editprofile.fxml", (Stage) editProfileButton.getScene().getWindow());
+        Utility.loadPage("/com/example/Authentication/editprofile.fxml", (Stage) editProfileButton.getScene().getWindow());
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
+
+    }
 }
+
