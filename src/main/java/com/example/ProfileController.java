034f3fdd855e790a3a79faf2380dfa11dd8d3f05
aluda <aluda.galla@ucalgary.ca>
2024-11-21 00:45:12 -0700

    fixed "log out" on profile page and added a new image to file

--- src/main/java/com/example/ProfileController.java
+++ src/main/java/com/example/ProfileController.java
@@ -5,13 +5,18 @@ import javafx.collections.ObservableArray;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.MenuItem;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -20,6 +25,22 @@ public class ProfileController implements Initializable {
     private Button backButton;
     @FXML
     private Button editProfileButton;
+    @FXML
+    private Label friendCountLabel;
+    @FXML
+    private ImageView profileAvatarImageView;
+    @FXML
+    private ListView<String> gameHistoryListView;
+    @FXML
+    private MenuBar menuBar;
+    @FXML
+    private MenuItem logoutMenuItem;
+
+    private final String[] gameHistoryData = {
+            "Checkers: You Won!",
+            "Tic Tac Toe: You Lost!",
+            "Chess: It Was A Tie!"
+    };
 
     @FXML
     private void toHomePage() {
@@ -31,8 +52,33 @@ public class ProfileController implements Initializable {
         Utility.loadPage("/com/example/Authentication/editprofile.fxml", (Stage) editProfileButton.getScene().getWindow());
     }
 
+    @FXML
+    private void toLoginPage(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("Are you sure you want to log out?");
+
+        alert.showAndWait().ifPresent(response -> {
+            if (response == ButtonType.OK) {
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/Authentication/login.fxml"));
+                    Parent root = loader.load();
+                    // Get the current stage from the event
+                    Stage stage = (Stage) menuBar.getScene().getWindow();
+                    Scene loginScene = new Scene(root);
+                    stage.setScene(loginScene);
+                    stage.show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        gameHistoryListView.getItems().addAll(gameHistoryData);
 
 
     }
