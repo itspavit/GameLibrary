d5c305b341190191b5491af24b5108b947d20ec0
Ezra Chin <ezra.chin@ucalgary.ca>
2024-11-23 18:56:55 -0700

    Completed Bishop test cases

--- Code/tests/GameLogic/Chess/ChessBoardAdvancedTests.java
+++ Code/tests/GameLogic/Chess/ChessBoardAdvancedTests.java
@@ -134,7 +134,7 @@ public class ChessBoardAdvancedTests {
     }
 
     @Test
-    public void RookMoveHorizontalU(){
+    public void RookMoveVerticalU(){
         coordinate = new Coordinate(0, 0);
         coordinate2 = new Coordinate(0, 7);
         Rook rook = new Rook(coordinate, true, board, -1000);
@@ -145,7 +145,7 @@ public class ChessBoardAdvancedTests {
     }
 
     @Test
-    public void RookMoveHorizontalD(){
+    public void RookMoveVerticalD(){
         coordinate = new Coordinate(0, 7);
         coordinate2 = new Coordinate(0, 0);
         Rook rook = new Rook(coordinate, true, board, -1000);
@@ -213,8 +213,106 @@ public class ChessBoardAdvancedTests {
         assertSame(board.GetGameStatus(), EGameState.P2_IN_CHECK);
     }
 
+    @Test
+    public void BishopMoveUR(){
+        coordinate = new Coordinate(0, 0);
+        coordinate2 = new Coordinate(7, 7);
+        Bishop bishop= new Bishop(coordinate, true, board, 32);
+        assertSame(board.GetPieceType(coordinate), EPieceType.BISHOP);
+        board.AttemptMove(coordinate, coordinate2, true);
+        assertSame(board.GetPieceType(coordinate), EPieceType.NOTHING);
+        assertSame(board.GetPieceType(coordinate2), EPieceType.BISHOP);
+    }
 
+    @Test
+    public void BishopMoveUL(){
+        coordinate = new Coordinate(7, 0);
+        coordinate2 = new Coordinate(0, 7);
+        Bishop bishop= new Bishop(coordinate, true, board, 32);
+        assertSame(board.GetPieceType(coordinate), EPieceType.BISHOP);
+        board.AttemptMove(coordinate, coordinate2, true);
+        assertSame(board.GetPieceType(coordinate), EPieceType.NOTHING);
+        assertSame(board.GetPieceType(coordinate2), EPieceType.BISHOP);
+    }
 
+    @Test
+    public void BishopMoveDR(){
+        coordinate = new Coordinate(0, 7);
+        coordinate2 = new Coordinate(7, 0);
+        Bishop bishop= new Bishop(coordinate, true, board, 32);
+        assertSame(board.GetPieceType(coordinate), EPieceType.BISHOP);
+        board.AttemptMove(coordinate, coordinate2, true);
+        assertSame(board.GetPieceType(coordinate), EPieceType.NOTHING);
+        assertSame(board.GetPieceType(coordinate2), EPieceType.BISHOP);
+    }
 
+    @Test
+    public void BishopMoveDL(){
+        coordinate = new Coordinate(7, 7);
+        coordinate2 = new Coordinate(0, 0);
+        Bishop bishop= new Bishop(coordinate, true, board, 32);
+        assertSame(board.GetPieceType(coordinate), EPieceType.BISHOP);
+        board.AttemptMove(coordinate, coordinate2, true);
+        assertSame(board.GetPieceType(coordinate), EPieceType.NOTHING);
+        assertSame(board.GetPieceType(coordinate2), EPieceType.BISHOP);
+    }
+
+    @Test
+    public void BishopMoveInvalid(){
+        coordinate = new Coordinate(0, 0);
+        coordinate2 = new Coordinate(0, 1);
+        Bishop bishop= new Bishop(coordinate, true, board, 32);
+        assertSame(board.GetPieceType(coordinate), EPieceType.BISHOP);
+        assertSame(board.AttemptMove(coordinate, coordinate2, true), EMoveResult.OUT_OF_PIECE_RANGE);
+    }
+
+    @Test
+    public void BishopMoveBlocked(){
+        coordinate = new Coordinate(0, 0);
+        coordinate2 = new Coordinate(1, 1);
+        Bishop bishop= new Bishop(coordinate, true, board, 32);
+        Pawn pawn2 = new Pawn(coordinate2, true, board, -1000);
+        assertSame(board.AttemptMove(coordinate, coordinate2, true), EMoveResult.PLACE_ON_OWN_PIECE);
+    }
+
+    @Test
+    public void BishopMoveBlocked2(){
+        coordinate = new Coordinate(0, 0);
+        coordinate2 = new Coordinate(1, 1);
+        Bishop bishop= new Bishop(coordinate, true, board, 32);
+        Pawn pawn2 = new Pawn(coordinate2, true, board, -1000);
+        coordinate2 = new Coordinate(7, 7);
+        assertSame(board.AttemptMove(coordinate, coordinate2, true), EMoveResult.OUT_OF_PIECE_RANGE);
+    }
+
+    @Test
+    public void BishopMoveCapture(){
+        coordinate = new Coordinate(0, 0);
+        coordinate2 = new Coordinate(5, 5);
+        Bishop bishop= new Bishop(coordinate, true, board, 32);
+        Pawn pawn2 = new Pawn(coordinate2, false, board, -1000);
+        board.AttemptMove(coordinate, coordinate2, true);
+        assertSame(board.GetPieceType(coordinate), EPieceType.NOTHING);
+        assertSame(board.GetPieceType(coordinate2), EPieceType.BISHOP);
+    }
+
+    @Test
+    public void BishopMoveBlockedBlack(){
+        coordinate = new Coordinate(0, 0);
+        coordinate2 = new Coordinate(1, 1);
+        Bishop bishop= new Bishop(coordinate, true, board, 32);
+        Pawn pawn2 = new Pawn(coordinate2, false, board, -1000);
+        coordinate2 = new Coordinate(7, 7);
+        assertSame(board.AttemptMove(coordinate, coordinate2, true), EMoveResult.OUT_OF_PIECE_RANGE);
+    }
+
+    @Test
+    public void BishopCheck(){
+        coordinate = new Coordinate(0, 0);
+        coordinate2 = new Coordinate(7, 7);
+        Bishop bishop= new Bishop(coordinate, true, board, 32);
+        King king2 = new King(coordinate2, false, board, 8);
+        assertSame(board.GetGameStatus(), EGameState.P2_IN_CHECK);
+    }
 
 }
